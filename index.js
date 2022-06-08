729aa940bb195a0bd9f034556632ff94f90ad37f
Kidd Evangelista <kiddevangelista@gmail.com>
2022-06-08 09:53:55 +0400

    add ep releases to tweet options; add album covers for reference

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const lyricsParse       = require('lyrics-parse')
+const lyricsParse       = require('lyrics-parse');
 const Twitter           = require('twitter');
 const { MusicBrainz }   = require('discography');
+const coverArtUrl       = require('coverarturl');
+const fs                = require('fs');
 
 require('dotenv').config();
 
@@ -24,18 +26,19 @@ const main = async () => {
     
     // initialize MusicBrainz API and album IDs
     const client = new MusicBrainz();
+    // const res = await client.getArtistDiscography("Turnover")
+    // console.log(res);
     
-    const day = new Date().getDate();
     let albumIDs = [
-        'd8c8ebb3-f7a7-4ef2-9440-fffa9fad72cd', // Self-titled
-        'a072280a-897f-4eb7-9131-09adf446d5b6', // Magnolia
-        '9d03ba97-d11c-4c60-92aa-432bb5d30701', // Peripheral Vision
-        'f659b650-8577-466f-bb60-74e9610256de', // Good Nature
-        '480175ce-0e3c-43cd-9c86-541722fd5b4c', // Altogether
+        {id: 'd8c8ebb3-f7a7-4ef2-9440-fffa9fad72cd', img: './img/st.jpg'}, // Self-titled EP
+        {id: 'a072280a-897f-4eb7-9131-09adf446d5b6', img: './img/mag.jpg'}, // Magnolia
+        {id: '9d03ba97-d11c-4c60-92aa-432bb5d30701', img: './img/pv.jpg'}, // Peripheral Vision
+        {id: 'f659b650-8577-466f-bb60-74e9610256de', img: './img/gn.jpg'}, // Good Nature
+        {id: '480175ce-0e3c-43cd-9c86-541722fd5b4c', img: './img/alt.jpg'}, // Altogether
+        {id: '83fc9f6a-d9c9-4fe2-ac1c-77a3efb42a93', img: './img/wtl.jpg'}, // Wait Too Long EP
+        {id: '4c68c52a-e8c2-40ec-8924-3243202ca3e1', img: './img/hp.jpg'}, // Humblest Pleasures EP
+        {id: 'e89c8067-0cb2-4eff-b82b-ab3c22905344', img: './img/bd.jpg'}, // Blue Dream EP
     ];
-    
-    // If date is visible by 3, add Blue Dream to rotation (this is added to avoid spamming of blue dream lyrics)
-    if (day % 3 === 0) albumIDs.push('e89c8067-0cb2-4eff-b82b-ab3c22905344'); // Blue Dream
 
     // shuffle album IDs twice
     albumIDs = shuffleArray(shuffleArray(albumIDs));
@@ -44,7 +47,7 @@ const main = async () => {
         try {
             // randomize turnover album and get tracks
             const album = albumIDs[parseInt(Math.random()*albumIDs.length)];
-            let tracks = await client.getTracksByReleaseGroup(album);
+            let tracks = await client.getTracksByReleaseGroup(album.id);
             tracks = shuffleArray(shuffleArray(tracks.map(track=>track=track.title)));
 
             // randomize song from chosen album and get lyrics
@@ -54,11 +57,24 @@ const main = async () => {
 
             // split the song into lines then get 4 lines of random lyrics
             let lyricsArr = lyrics.split("\n");
+
+            // if lyrics cant be parsed by new line then parse by comma and period
+            if(lyricsArr.length === 1) lyricsArr = lyricsArr[0].split(/[,.]+/)
+
+            // filter empty strings
             lyricsArr = lyricsArr.filter(e=>e!=='');
+
             const maxLine = lyricsArr.length;
             let index = parseInt(Math.random()*maxLine);
             index = (index+4 > maxLine)? maxLine-4:index;
-            const msg = lyricsArr.slice(index, index+4).join('\n').replace(/&#8217;/g, '\'');
+
+            // capitalize all first letters of the line
+            lyricsArr = lyricsArr.map(lyr => {
+                const tempLyr = lyr.trim().replace(/&#8217;/g, '\'').replace(/<\/?[^>]+(>|$)/g, '');
+                return tempLyr.charAt(0).toUpperCase() + tempLyr.slice(1)
+            })
+            // join lyrics array
+            const msg = lyricsArr.slice(index, index+4).join('\n');
             console.log(msg);
 
             // initialize Twitter API
@@ -69,8 +85,10 @@ const main = async () => {
                 access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
             });
 
-            // Post lyrics
-            await twitterClient.post('statuses/update', {status: msg});
+            // Get image then post lyrics
+            const imageData = fs.readFileSync(album.img)
+            const { media_id_string } = await twitterClient.post("media/upload", {media: imageData})
+            await twitterClient.post('statuses/update', {status: msg, media_ids: media_id_string});
             console.log('Tweeted successfully');
             break;
         }
